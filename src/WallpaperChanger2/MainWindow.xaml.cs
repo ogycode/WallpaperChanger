928d2eb314f8e0d728f9b1ed138a612b3d17b1ac
ogycode <verlokav@gmail.com>
2017-06-04 15:06:14 +0300

    Added: Light theme and some functionaly.

--- src/WallpaperChanger2/MainWindow.xaml.cs
+++ src/WallpaperChanger2/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Diagnostics;
 using System.IO;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Timers;
 using System.Windows;
@@ -20,12 +21,13 @@ namespace WallpaperChanger2
         const int SPIF_SENDWININICHANGE = 0x02;
 
         Timer timer;
-        
+        System.Windows.Forms.NotifyIcon notifyIcon;
+
         public MainWindow()
         {
             InitializeComponent();
         }
-        
+
         public void SetupImage(Uri uri, Model.Style style)
         {
             Stream s = new System.Net.WebClient().OpenRead(uri.ToString());
@@ -57,70 +59,37 @@ namespace WallpaperChanger2
         }
         public async void Build()
         {
-            LOG("Start build....");
-
             string uid = App.Settings.GetValue("ImageUID", "");
 
             string url = await Bing.ImageUrl();
 
-            if(CheckDate())
-            {
-                LOG("CheckDate() returned TRUE");
-
+            if (CheckDate())
                 if (uid != url)
                 {
-                    LOG("ImageUID - uid != url");
-
                     SetupImage(new Uri(url, UriKind.RelativeOrAbsolute), Model.Style.Stretched);
-
-                    LOG($"Setuped new image: {url}");
-
                     var dt = DateTime.Now.Add(new TimeSpan(24, 0, 0));
 
+                    App.Settings["UpdateWallpaperYear"] = dt.Year;
                     App.Settings["UpdateWallpaperMounth"] = dt.Month;
                     App.Settings["UpdateWallpaperDay"] = dt.Day;
                     App.Settings["UpdateWallpaperHour"] = dt.Hour;
 
                     App.Settings["ImageUID"] = url;
                 }
-                else
-                {
-                    LOG("ImageUID - uid == url");
-                }
-            }
-            else
-            {
-                LOG("CheckDate() returned FALSE");
-            }
         }
         public bool CheckDate()
         {
-            int mounth = DateTime.Now.Month;
-            int day = DateTime.Now.Day;
-            int hour = DateTime.Now.Hour;
+            DateTime dt = new DateTime(App.Settings.GetValue("UpdateWallpaperYear", 2000), 
+                                       App.Settings.GetValue("UpdateWallpaperMounth", 1), 
+                                       App.Settings.GetValue("UpdateWallpaperDay", 1), 
+                                       App.Settings.GetValue("UpdateWallpaperHour", 1), 0, 0);
 
-            LOG($"Date: Mounth - {App.Settings.GetValue("UpdateWallpaperMounth", 0)}/{mounth} Day - {App.Settings.GetValue("UpdateWallpaperDay", 0)}/{day} Hour - {App.Settings.GetValue("UpdateWallpaperHour", 0)}/{hour}");
 
-            if (mounth >= App.Settings.GetValue("UpdateWallpaperMounth", 0))
-                if (day >= App.Settings.GetValue("UpdateWallpaperDay", 0))
-                    if (hour > App.Settings.GetValue("UpdateWallpaperHour", 0))
-                        return true;
-
-            return false;
+            return (DateTime.Now > dt);
         }
         double GetMilisec(int minunte)
         {
             return 60000 * minunte;
-        }
-
-        public static void LOG(string log)
-        {
-            string path = @"C:\Users\Верлока Вадим\Desktop\log.txt";
-            
-            using (StreamWriter sw = File.AppendText(path))
-            {
-                sw.WriteLine($"{DateTime.Now.ToShortDateString()} {DateTime.Now.ToShortTimeString()}: {log}");
-            }
         }
 
         #region Window Events
@@ -131,16 +100,14 @@ namespace WallpaperChanger2
         }
         private void btnCloseClick()
         {
-            /*if (App.Settings.GetValue<bool>("AppExit"))
+            if (App.Settings.GetValue<bool>("AppExit"))
             {
                 Application.Current.Shutdown(0);
             }
             else
             {
                 Hide();
-            }*/
-
-            Application.Current.Shutdown(0);
+            }
         }
         private void btnMinimazeClick()
         {
@@ -154,13 +121,90 @@ namespace WallpaperChanger2
 
         private void mywindowLoaded(object sender, RoutedEventArgs e)
         {
+            //timer
             timer = new Timer(GetMilisec(60));
             timer.Elapsed += TimerElapsed;
             timer.Enabled = true;
 
-            LOG("Application loaded");
+            //notify
+            notifyIcon = new System.Windows.Forms.NotifyIcon();
+            notifyIcon.Text = "Wallpaper Changer 2";
+            notifyIcon.Icon = Properties.Resources.AppIcon;
+            notifyIcon.Visible = true;
+            notifyIcon.DoubleClick += NotifyIconDoubleClick;
+            notifyIcon.ContextMenuStrip = new System.Windows.Forms.ContextMenuStrip();
+            notifyIcon.ContextMenuStrip.Items.Add("Update wallpaper").Click += UpdateWallpaerClick;
+            notifyIcon.ContextMenuStrip.Items.Add("Exit").Click += ExitClick;
+
+            //app exit
+            Application.Current.Exit += CurrentExit;
+
+            //startup
+            RegistryKey key = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            if ((string)key.GetValue("Wallpaper Changer 2") == null)
+                cbStartup.IsChecked = false;
+            else
+                cbStartup.IsChecked = true;
+            cbStartup.Click += CbStartupClick;
+
+            //theme
+            cbTheme.SelectedIndex = App.Settings.GetValue("Theme", 0);
+            cbTheme.SelectionChanged += CbThemeSelectionChanged;
+
+            //app exit when collsing
+            cbExit.IsChecked = App.Settings.GetValue("AppExit", false);
+            cbExit.Click += CbExitClick;
+
+            //show msg
+            cbAlertInternet.IsChecked = App.Settings.GetValue("ShowAlertInternetMsg", false);
+            cbAlertInternet.Click += CbAlertInternetClick;
 
             Build();
+        }
+
+        private void CbAlertInternetClick(object sender, RoutedEventArgs e)
+        {
+            App.Settings["ShowAlertInternetMsg"] = cbAlertInternet.IsChecked.Value;
+        }
+        private void CbExitClick(object sender, RoutedEventArgs e)
+        {
+            App.Settings["AppExit"] = cbExit.IsChecked.Value;
+        }
+        private void CbThemeSelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            App.Settings["Theme"] = cbTheme.SelectedIndex;
+            App.UpdateTheme(cbTheme.SelectedIndex);
+            UpdateDefaultStyle();
+        }
+        private void CbStartupClick(object sender, RoutedEventArgs e)
+        {
+            RegistryKey key = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            if (!cbStartup.IsChecked.Value)
+                key.DeleteValue("Wallpaper Changer 2", false);
+            else
+                key.SetValue("Wallpaper Changer 2", $"\"{Assembly.GetExecutingAssembly().Location}\" -silent");
+        }
+        private void CurrentExit(object sender, ExitEventArgs e)
+        {
+            notifyIcon.Visible = false;
+            notifyIcon.Dispose();
+            notifyIcon = null;
+            timer.Dispose();
+            timer = null;
+        }
+        private void ExitClick(object sender, EventArgs e)
+        {
+            Application.Current.Shutdown(0);
+        }
+        private void UpdateWallpaerClick(object sender, EventArgs e)
+        {
+            //TODO
+        }
+        private void NotifyIconDoubleClick(object sender, EventArgs e)
+        {
+            Show();
+            if (WindowState == WindowState.Minimized)
+                WindowState = WindowState.Normal;
         }
         private void TimerElapsed(object sender, ElapsedEventArgs e)
         {
