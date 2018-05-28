81fb58ce37eabc1eaab7d4c02a7d542b5af0f127
ogycode <verlokav@gmail.com>
2018-05-28 11:39:55 +0300

    Add method for hide setting button when selected wallpaper source.

---
+++ src/WallpaperChanger/WallpaperChanger/Core/SelectedIndexToVisibilityConvert.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows;
+
+namespace WallpaperChanger.Core
+{
+    public class SelectedIndexToVisibilityConvert : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int i = (int)value;
+
+            if (i == 2)// || i == 5
+                return Visibility.Visible;
+            else
+                return Visibility.Collapsed;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => (Visibility)value == Visibility.Visible ? 2 : -1;
+    }
+}
